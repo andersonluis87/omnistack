4cbc888726c8f9df429ae07bb0016b27808e3e1f
Anderson Luís Silva <andersonluis87@gmail.com>
2020-01-18 18:26:01 -0300

    Backend em Desenvolvimento

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+
+module.exports = {
+    async store(request, response) {
+        const {github_username, techs, latitude, longitude } = request.body;
+        
+        const githubApiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+    
+        const { name = login, avatar_url, bio} = githubApiResponse.data;
+    
+        const techsArray = techs.split(',').map(tech => tech.trim());
+    
+        const location = {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+        }
+    
+        const dev = await Dev.create({
+            github_username,
+            name,
+            avatar_url,
+            bio,
+            techs: techsArray,
+            location
+        });
+    
+        return response.json(dev);
+    }
+}
