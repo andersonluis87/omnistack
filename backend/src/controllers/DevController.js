de201a628847715fd90a133a2346488f8a6e9a1c
Anderson Luís Silva <andersonluis87@gmail.com>
2020-01-19 15:23:20 -0300

    Backend finalizado
    CRUD completo

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,22 +1,33 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+// index, show, store, update, destroy
 
 module.exports = {
+    async index(request, response) {
+        const devs = await Dev.find();
+
+        return response.json(devs);
+    },
     async store(request, response) {
         const {github_username, techs, latitude, longitude } = request.body;
         
+        let dev = await Dev.findOne({github_username});
+
+        if(dev) 
+            return response.status(403).send('This user already exists');
+        
+        
         const githubApiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-    
         const { name = login, avatar_url, bio} = githubApiResponse.data;
-    
-        const techsArray = techs.split(',').map(tech => tech.trim());
-    
+        const techsArray = parseStringAsArray(techs);
         const location = {
             type: 'Point',
             coordinates: [longitude, latitude],
         }
     
-        const dev = await Dev.create({
+        dev = await Dev.create({
             github_username,
             name,
             avatar_url,
@@ -26,5 +37,38 @@ module.exports = {
         });
     
         return response.json(dev);
+    },
+    async update(request, response) {
+        const github_username = request.params.id;
+        const { name, bio, techs } = request.body;
+
+        let dev = await Dev.findOne({github_username});
+
+        if (!dev)
+            return response.status(404).send('Dev not found');
+
+        dev.name = name;
+        dev.bio = bio;
+        dev.techs = parseStringAsArray(techs);
+
+        await dev.save();
+        
+        return response.status(200).send('Dev updated successfully');   
+    },
+    async destroy(request, response) {
+        const { id } = request.params;
+        
+        console.log(request.params)
+
+        const dev = await Dev.findById(id);
+
+        console.log(dev);
+        
+        if (!dev)
+            return response.status(404).send('Dev not found');
+
+        await Dev.deleteOne({ _id: id });
+        
+        return response.status(200).send('Dev removed successfully');
     }
 }
